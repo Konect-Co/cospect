39002759f21a5a8950c57faf6c490e9e00840994
Ravit Sharma <sharma.ravit@gmail.com>
2020-05-15 15:13:46 -0700

    Changes to web app. Changing training file

--- train-model/train.py
+++ train-model/train.py
@@ -6,61 +6,35 @@ import os
 import librosa
 
 from openpyxl import load_workbook
-pd.DataFrame()
 
-class MakeData:
-   def __init__(self,audio_path,json_path):
-       self.audio_path=audio_path
-       self.json_path=json_path
-   
-   def is_json(self,filename):
-       try:
-           x=str(filename)
-           y=x.split('.')[1]
-           if(y=='json'):
-               
-               return True
-       except IndexError as e:
-           print('FILE EXTENSION NOT IN '+'.json')
-       
-   def make_df(self,csv_name='audio.csv'):
-       df=pd.DataFrame()
-       json_files=os.listdir(self.json_path)
-       audio_files=os.listdir(self.audio_path)
-       
-       df=pd.DataFrame(columns=['id','age','gender','symptoms','disease','audio','sampling rate','time'])
-       i=0
-       #getting names of  all the keys in json file
-       for j in json_files:
-           if(self.is_json(j)==True):
-               with open(os.path.join(self.json_path,j)) as f:
-                   dict1=json.load(f)
-                   id1=j.split('.')[0]
-                   age=dict1['age']
-                   gender=dict1['gender']
-                   symptoms=dict1['symptoms'][0]
-                   disease=dict1['disease']
-               
-                   df.loc[i,'id']=id1
-                   df.loc[i,'age']=age
-                   df.loc[i,'gender']=gender
-                   df.loc[i,'symptoms']=symptoms
-                   df.loc[i,'disease']=disease
-                   path=self.audio_path+'/'+j.split('.')[0]+'.wav'
-                   audio,sample=librosa.core.load(path=os.path.normpath(path))
-                   
-                   df.loc[i,'audio']=np.asarray(audio)
-                   df.loc[i,'sampling rate']=sample
-                   
-                   duration=librosa.get_duration(y=audio,sr=sample)
-                   df.loc[i,'time']=duration
-                   
-                   i=i+1
-       return df
-#%%
 import ast
 from pydub import AudioSegment
+#%%
+#TODO: This method is repeated in several locations, including "GenData/DataAugmentation"
+#   Consolidate into one Python util file
+#utility function to create spectrogram input from an audio file
 
+def make_spectrogram(signal, sample_rate):
+	resample_freq = 8000
+	signal_resampled = librosa.core.resample(signal, sample_rate, resample_freq)
+
+	stft = librosa.core.stft(signal_resampled)
+	spectrogram = np.power(np.abs(stft), 0.5)
+	
+	spectrogram = np.swapaxes(spectrogram, 0, 1)
+    
+	return spectrogram
+
+def file2spectrogram(file_path):
+	signal, sample_rate = librosa.core.load(file_path)
+	if signal.shape[0] == 2:
+		signal = np.mean(signal, axis=0)
+
+	spectrogram = make_spectrogram(signal, sample_rate)
+	
+	return spectrogram
+
+#%%
 def make_df():
     df=pd.DataFrame(columns=['id','age','gender','symptoms','disease','audio','sampling rate','time'])
     
@@ -72,12 +46,15 @@ def make_df():
     wb = load_workbook(sheet_path)
     ws = wb.active
     
-    start_row = 7
-    end_row = 11
+    start_row = 8
+    end_row = 8
+
+    x_spect_data = []
+    y_num_data = []
 
     for row_i in range(start_row, end_row+1):
         name = str(row_i)
-        yt_link = ws["H" + str(row_i)].value
+        yt_link = ws["H" + name].value
         
         file_path = base_path + "/cospect/Data/temp"
         os.system("youtube-dl --extract-audio " + yt_link + " -o " + file_path + ".wav")
@@ -90,46 +67,58 @@ def make_df():
         elif m4a_ext:
             dl_path = file_path + ".m4a"
 	
-        out_path = file_path + ".wav"
-
+        #out_path = file_path + ".wav"
+        new_sound = AudioSegment.empty()
         if opus_ext or m4a_ext:
-            cmdstr = "ffmpeg -i \"" + dl_path + "\" -f wav -flags bitexact \"" + out_path + "\""		
-            os.system(cmdstr)
-            os.remove(dl_path)
-            
+            #cmdstr = "ffmpeg -i \"" + dl_path + "\" -f wav -flags bitexact \"" + out_path + "\""		
+            #print(cmdstr)
+            #os.system(cmdstr)
+            #os.remove(dl_path)
             section = ws["I" + str(row_i)].value
             if(section != "FULL"):
-                sound = AudioSegment.from_wav(out_path)
-                
+                print("file downloaded")
+                sound = AudioSegment.from_file(dl_path)
+                print("file loaded")
                 section = ast.literal_eval(section) #string representation to actual list
-                new_sound = AudioSegment.empty()
                 section.reverse() #to make intervals from lastest to earliest
+                
                 for interval in section: #want to consider the intervals from largest to smallest
                     new_sound += sound[interval[0]*1000:interval[1]*1000] #changing seconds to milliseconds
-                
-                os.remove(out_path)
-                new_sound.export(out_path)
+                print("file spliced")
+                os.remove(dl_path) #outpath
+                #new_sound.export(dl_path) #outpath
         
         gender = ws["B" + str(row_i)].value
         age = ws["C" + str(row_i)].value
         symptoms = ws["D" + str(row_i)].value.split("_")
         disease = ws["G" + str(row_i)].value
-            
-        df.loc[row_i,'id']=name
+        
+        """df.loc[row_i,'id']=name
         df.loc[row_i,'age']=age
         df.loc[row_i,'gender']=gender
         df.loc[row_i,'symptoms']=symptoms
         df.loc[row_i,'disease']=disease
         
-        audio,sample=librosa.core.load(path=out_path)
+        df.loc[row_i,'sampling rate']=new_sound.frame_rate
                            
-        df.loc[row_i,'audio']=np.asarray(audio)
-        df.loc[row_i,'sampling rate']=sample
-                           
-        duration=librosa.get_duration(y=audio,sr=sample)
-        df.loc[row_i,'time']=duration
+        df.loc[row_i,'time']=new_sound.duration_seconds
         
-        return df
+        print("fields saved")
+        
+        df.loc[row_i,'audio']=new_sound
+        
+        print("audio saved")"""
+        
+
+        spectrogram = make_spectrogram(np.asarray(new_sound), new_sound.frame_rate)
+        x_spect_data.append(spectrogram)
+
+        if ("Dry" in symptoms):
+            y_num_data.append(1)
+        else:
+            y_num_data.append(0)
+        
+    return x_spect_data, y_num_data
     
 #%%
 df = make_df()
@@ -167,30 +156,7 @@ def get_model():
     	
     return model
     
-#%%
-#TODO: This method is repeated in several locations, including "GenData/DataAugmentation"
-#   Consolidate into one Python util file
-#utility function to create spectrogram input from an audio file
 
-def make_spectrogram(signal, sample_rate):
-	resample_freq = 8000
-	signal_resampled = librosa.core.resample(signal, sample_rate, resample_freq)
-
-	stft = librosa.core.stft(signal_resampled)
-	spectrogram = np.power(np.abs(stft), 0.5)
-	
-	spectrogram = np.swapaxes(spectrogram, 0, 1)
-    
-	return spectrogram
-
-def file2spectrogram(file_path):
-	signal, sample_rate = librosa.core.load(file_path)
-	if signal.shape[0] == 2:
-		signal = np.mean(signal, axis=0)
-
-	spectrogram = make_spectrogram(signal, sample_rate)
-	
-	return spectrogram
 #%%
 from keras.optimizers import Adam    
 
@@ -259,8 +225,8 @@ for i in range(len(x_spect_data)):
     x_sample_padded = np.pad(x_sample, [(0, max_len-x_sample_len), (0,0)])
     x_padded[i] = x_sample_padded
 #%%
-print(y_data_split)
-training_x, testing_x, training_y, testing_y = train_test_split(x_spect_data, y_num_data, test_size=0.10)
+print(len(y_num_data))
+#training_x, testing_x, training_y, testing_y = train_test_split(x_spect_data, y_num_data, test_size=0.10)
 #%%
 print(len(training_x))
 
--- webapp/Server.js
+++ webapp/Server.js
@@ -14,54 +14,53 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 var server = function(req, res, next) {
-	if (req.method == 'POST') {
+	/*if (req.method == 'POST') {
 		var form = new formidable.IncomingForm();
 
 		form.parse(req, function(err, fields, files) {
 			console.log(fields);
 			console.log(files);
+
+			var name = fields['fname'];
+			var sex = fields['sex'];
+			var age = fields['age'];
+			var status = fields['status'];
+			var comments = fields['comments'];
+
+			console.log(req.body);
+			console.log(name);
+			console.log(sex);
+			console.log(age);
+			console.log(status);
+			console.log(comments);
+
+			console.log(req.body.status);
+
+			var id = uuid.v4();
+			var base_path = "/home/ravit/Konect-Code/cospect/webapp/";
+
+			//TODO: mkdir id
+			fs.mkdirSync("audio_files/" + id, {recursive: true});
+
+			var audioPath = "./audio_files/" + id + "/" + id + ".wav";
+			//TODO: Get audio body from req
+			//TODO: write audio file to audioPath
+
+			var json_obj = {
+				"id": id,
+				"name": name,
+				"sex": sex,
+				"age": age,
+				"status": status,
+				"comments": comments
+			};
+
+			var jsonPath = base_path + "audio_files/" + id + "/info.json";
+			fs.writeFileSync(jsonPath, JSON.stringify(json_obj));
 		});
-
-		console.log("POST " + req.url);
-		console.log(req.body.status);									
-		
-		var name = "";
-		var sex = "";
-		var age = "";
-		var status = "";
-		var comments = "";
-
-		console.log(req.body);
-		console.log(name);
-		console.log(sex);
-		console.log(age);
-		console.log(status);
-		console.log(comments);
-
-		var id = uuid.v4();
-
-		//TODO: mkdir id
-		fs.mkdirSync("audio_files/" + id);
-
-		var audioPath = "audio_files/" + id + "/" + id + ".wav";
-		//TODO: Get audio body from req
-		//TODO: write audio file to audioPath
-
-		var json_obj = {
-			"id": id,
-			"name": name,
-			"sex": sex,
-			"age": age,
-			"status": status,
-			"comments": comments
-		};
-
-		var jsonPath = "audio_files/" + id + "/" + "info.json";
-		fs.writeFileSync(jsonPath, JSON.stringify(json_obj));
-
 		res.end();
-	}
-	else if (req.method == "GET") {
+	}*/
+	if (req.method == "GET") {
 		console.log("GET " + req.url);
 		if (req.url == "/" || req.url == "") {
 			fs.readFile("./index.html", function (err, html) {
@@ -74,10 +73,10 @@ var server = function(req, res, next) {
 			});
 		}
 		else {
-			fs.readFile(req.url, function (err, html) {
-				if (err) {	return;	}
-				res.writeHeader(200, {"Content-Type": "text/html"});
-				res.write(html);
+			fs.readFile("." + req.url, function (err, content) {
+				if (err) { res.end(); return; }
+				res.writeHeader(200);
+				res.write(content);
 				res.end();
 			});
 		}
