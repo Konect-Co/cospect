eb342332218064766204486d3b2cafb70bbe65c8
Ravit Sharma <sharma.ravit@gmail.com>
2020-05-20 08:01:52 -0700

    Change training functionality

--- train-model/train.py
+++ train-model/train.py
@@ -1,4 +1,3 @@
-import librosa as lb
 import json
 import numpy as np
 import pandas as pd
@@ -31,12 +30,10 @@ def file2spectrogram(file_path):
 		signal = np.mean(signal, axis=0)
 
 	spectrogram = make_spectrogram(signal, sample_rate)
-	
 	return spectrogram
 
-#%%
-def make_df():
-    df=pd.DataFrame(columns=['id','age','gender','symptoms','disease','audio','sampling rate','time'])
+def make_data():
+    #df=pd.DataFrame(columns=['id','age','gender','symptoms','disease','audio','sampling rate','time'])
     
     dirs = "/home/ravit/Konect-Code".split("/")
     base_path = "/".join(dirs)
@@ -46,10 +43,10 @@ def make_df():
     wb = load_workbook(sheet_path)
     ws = wb.active
     
-    start_row = 8
-    end_row = 8
+    start_row = 14
+    end_row = 20
 
-    x_spect_data = []
+    x_audio_data = []
     y_num_data = []
 
     for row_i in range(start_row, end_row+1):
@@ -57,7 +54,7 @@ def make_df():
         yt_link = ws["H" + name].value
         
         file_path = base_path + "/cospect/Data/temp"
-        os.system("youtube-dl --extract-audio " + yt_link + " -o " + file_path + ".wav")
+        os.system("youtube-dl --extract-audio " + yt_link + " -o" + file_path + ".wav")
         
         opus_ext = os.path.exists(file_path + ".opus")
         m4a_ext = os.path.exists(file_path + ".m4a")
@@ -67,66 +64,92 @@ def make_df():
         elif m4a_ext:
             dl_path = file_path + ".m4a"
 	
-        #out_path = file_path + ".wav"
-        new_sound = AudioSegment.empty()
         if opus_ext or m4a_ext:
-            #cmdstr = "ffmpeg -i \"" + dl_path + "\" -f wav -flags bitexact \"" + out_path + "\""		
-            #print(cmdstr)
-            #os.system(cmdstr)
-            #os.remove(dl_path)
+            spliced_sound = sound = AudioSegment.from_file(dl_path)
             section = ws["I" + str(row_i)].value
             if(section != "FULL"):
                 print("file downloaded")
-                sound = AudioSegment.from_file(dl_path)
-                print("file loaded")
                 section = ast.literal_eval(section) #string representation to actual list
                 section.reverse() #to make intervals from lastest to earliest
                 
                 for interval in section: #want to consider the intervals from largest to smallest
-                    new_sound += sound[interval[0]*1000:interval[1]*1000] #changing seconds to milliseconds
+                    spliced_sound += sound[interval[0]*1000:interval[1]*1000] #changing seconds to milliseconds
                 print("file spliced")
-                os.remove(dl_path) #outpath
-                #new_sound.export(dl_path) #outpath
-        
-        gender = ws["B" + str(row_i)].value
-        age = ws["C" + str(row_i)].value
-        symptoms = ws["D" + str(row_i)].value.split("_")
-        disease = ws["G" + str(row_i)].value
-        
-        """df.loc[row_i,'id']=name
-        df.loc[row_i,'age']=age
-        df.loc[row_i,'gender']=gender
-        df.loc[row_i,'symptoms']=symptoms
-        df.loc[row_i,'disease']=disease
-        
-        df.loc[row_i,'sampling rate']=new_sound.frame_rate
-                           
-        df.loc[row_i,'time']=new_sound.duration_seconds
-        
-        print("fields saved")
-        
-        df.loc[row_i,'audio']=new_sound
-        
-        print("audio saved")"""
-        
+                os.remove(dl_path)
+                #spliced_sound.export(dl_path)
 
-        spectrogram = make_spectrogram(np.asarray(new_sound), new_sound.frame_rate)
-        x_spect_data.append(spectrogram)
-
-        if ("Dry" in symptoms):
-            y_num_data.append(1)
-        else:
-            y_num_data.append(0)
-        
-    return x_spect_data, y_num_data
+            symptoms = ws["D" + str(row_i)].value.split("_")
+            print("symptoms read")
+            
+            #resample audio segment to specified sample_rate
+            sample_rate = 5000
+            spliced_sound.set_frame_rate(sample_rate)
+            
+            chunk_len = 10 #seconds each chunk should be
+            n = chunk_len*sample_rate
+            
+            #TODO: Not sure if this is being done correctly
+            #Intended functionality: Split into 10 second segments each
+            sound_array = spliced_sound.get_array_of_samples()
     
+            print("value of row_i", row_i)
+            print("sound_array LENGTH", len(sound_array))
+            print("value of n", n)
+            
+            chunks = [sound_array[t:t + n] for t in range(0, len(sound_array), n)]
+            
+            print("chunks LENGTH", len(chunks))
+            
+            #padding last chunk to correct size
+            chunks[-1] = np.append(chunks[-1], np.zeros((n-len(chunks[-1]))))
+            x_audio_data += chunks
+            
+            symptom = 1 if "Dry" in symptoms else 0
+            for _ in range(len(chunks)):
+                y_num_data.append(symptom)
+        
+    x_audio_data = np.asarray(x_audio_data, dtype=np.float32)
+    y_num_data = np.asarray(y_num_data, dtype=np.float32)
+    
+    return x_audio_data, y_num_data
 #%%
-df = make_df()
-print(df.head())
+x_audio_data, y_num_data = make_data()
+print(x_audio_data.shape)
+#%%
+from sklearn.model_selection import train_test_split
 
+training_x, testing_x, training_y, testing_y = train_test_split(x_audio_data, y_num_data, test_size=0.10)
+
+#%%
+
+"""
+STFT for TF vs Librosa: For Reference
+
+https://www.tensorflow.org/api_docs/python/tf/signal/stft
+https://librosa.github.io/librosa/generated/librosa.core.stft.html
+
+STFT parameters
+- Window size - (TF: frame_length, Librosa: win_length)
+- Length of windowed signal after padding - (TF: pad_end, Librosa: n_fft - little different functionality)
+- Step length- (TF: frame_step, Librosa: hop_length)
+- Windowing specification - (TF: window_fn, Librosa: window)
+
+Librosa defaults (needed for tf)
+- frame_length - 2048
+- frame_step - frame_length/4 = 512
+"""
 #%%
 from keras import Model
-from keras.layers import Input, Conv1D, RNN, SimpleRNNCell, Dense, Activation, SimpleRNN
+from keras.layers import Input, Conv1D, RNN, SimpleRNNCell, Dense, Activation, SimpleRNN, Lambda
+import tensorflow as tf
+
+def make_tf_spect(signals):
+    frame_length = tf.constant(2048)
+    fft_length = tf.constant(512)
+    spectrogram_complex = tf.signal.stft(signals, frame_length, fft_length)
+    spectrogram = tf.math.abs(spectrogram_complex)
+    spectrogram = tf.transpose(spectrogram, perm=[0,2,1])
+    return spectrogram
 
 def get_model():
     conv_filters = 100
@@ -134,12 +157,15 @@ def get_model():
     rnn_units = 100
     output_size = 1
     
-    input_layer = Input(shape=(None, 1025))
-    #input size is (None, None, 1025)
-    	
+    input_layer = Input(shape=(50000,), dtype=tf.float32)
+    #input size is (None, 50000)
+
+    #Lambda layer is necessary as tensorflow operations are involved
+    spectrogram = Lambda(lambda x:make_tf_spect(x))(input_layer)
+    
     #conv_output = Conv1D(conv_filters, conv_kernel_size, padding='causal')(input_layer)
     #causal padding not implemented for tensorflow js
-    conv_output = Conv1D(conv_filters, conv_kernel_size)(input_layer)
+    conv_output = Conv1D(conv_filters, conv_kernel_size)(spectrogram)
     #casual padding preserves time_steps dimension as the same
     	
     #rnn_output = RNN(SimpleRNNCell(rnn_units))(conv_output)
@@ -148,7 +174,7 @@ def get_model():
     	
     dense_output = Dense(output_size)(rnn_output)
     #output is (None, 1)
-    	
+    
     output_layer = Activation('softmax')(dense_output)
     #output is (None, 1)
 
@@ -158,6 +184,8 @@ def get_model():
     
 
 #%%
+model = get_model()
+
 from keras.optimizers import Adam    
 
 def train (training_x, training_y, testing_x, testing_y):
@@ -165,84 +193,24 @@ def train (training_x, training_y, testing_x, testing_y):
 	decay=1e-6
 	epochs=10
 
-	model = get_model()
-
 	opt = Adam(lr=lr, decay=decay)
 	model.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
 	model.fit(training_x, training_y, epochs=epochs, validation_data=(testing_x, testing_y))
-
-#%%
-from sklearn.model_selection import train_test_split
-
-#get defined values for training_x, training_y, testing_x, testing_y from pd dataframe
-x_audio_data = df['audio'].to_numpy()
-x_sampling_rate = df['sampling rate'].to_numpy()
-y_data = df['symptoms'].to_numpy()
-
-x_audio_split = []
-y_data_split = []
-
-chunk_len = 10 #seconds each chunk should be
-
-#%%
-
-x_spect_data = []
-for (audio_data, sampling_rate) in zip(x_audio_data, x_sampling_rate):
-    spectrogram = make_spectrogram(audio_data, sampling_rate)
-    x_spect_data.append(spectrogram)
-
-
-y_num_data = []
-for symptoms in y_data:
-    if ("Dry" in symptoms):
-        y_num_data.append(1)
-    else:
-        y_num_data.append(0)
-y_num_data = np.asarray(y_num_data)
-#%%
-for i in range(x_audio_data.shape[0]):
-    x_audio_sample = x_audio_data[i]
-    n = x_sampling_rate[i]*chunk_len
-    x_split = [x_audio_sample[t:t + n] for t in range(0, len(x_audio_sample), n)]
-    for _ in range(n-len(x_split[-1])):
-        x_split[-1] = np.append(x_split[-1], 0)
-    for j in range(len(x_split)):
-        y_data_split.append(y_data[i])
-    x_audio_split += x_split
-#%%
-num_samples = df.shape[0]
-
-max_len = 0
-for x_sample in x_spect_data:
-    length = x_sample.shape[0]
-    if length > max_len:
-        max_len = length
-
-x_padded = np.zeros((num_samples, max_len, 1025))
-for i in range(len(x_spect_data)):
-    x_sample = x_spect_data[i]
-    x_sample_len = x_sample.shape[0]
-    x_sample_padded = np.pad(x_sample, [(0, max_len-x_sample_len), (0,0)])
-    x_padded[i] = x_sample_padded
-#%%
-print(len(y_num_data))
-#training_x, testing_x, training_y, testing_y = train_test_split(x_spect_data, y_num_data, test_size=0.10)
-#%%
-print(len(training_x))
 
 #%%
 trained_model = train(training_x, training_y, testing_x, testing_y)
 #%%
 #Saving the model in Keras format
 #TODO: Save only the model and weights, not optimizer state or any of that junk
-"""trained_model.save("./model.h5")
+"""
 
-trained_model.save_weights('./model_weights.h5')
+model.save_weights('./model_weights.h5')
 with open("./model.json", 'w') as file:
-	file.write(model.to_json())"""
+	file.write(model.to_json())
 
-trained_model.save("./tf-model", save_format="tf")
+model.save("./tf-model")
 
 import tensorflow as tf
 model_tf = tf.keras.models.load_model("./model.h5")
 model_tf.save("./tf-model", save_format="tf")
+"""
